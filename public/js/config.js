aa1e14e0ec7f845653e8dc9d65c698882d23573e
Radu Vultur <raduvultur@gmail.com>
2013-11-20 09:38:00 -0500

    small changes

--- public/js/config.js
+++ public/js/config.js
@@ -10,7 +10,7 @@ window.app.config(['$routeProvider',
         }).
         when('/articles/:articleId/edit', {
             templateUrl: 'views/articles/edit.html'
-        }).
+        }).        
         when('/articles/:articleId', {
             templateUrl: 'views/articles/view.html'
         }).
